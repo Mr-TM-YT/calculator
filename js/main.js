77ca3fe8a508da16687ed60b6ae39c390e95c3e4
Mr-TM-YT <www.mohamedsalwa13@gmail.com>
2024-03-14 18:03:30 +0200

    Added some helper function and removed some testing code

--- js/main.js
+++ js/main.js
@@ -2,34 +2,46 @@ const numberButtons = document.querySelectorAll("button");
 let calculationSpan = document.getElementById("calc");
 let realCalculation = "";
 
-for (let numberButton of numberButtons) {
-  numberButton.addEventListener("click", () => {
-    if (
-      numberButton.value !== "=" &&
-      numberButton.innerText !== "AC" &&
-      numberButton.value !== "del"
-    ) {
-      if (numberButton.classList.contains("operator")) {
-        realCalculation += numberButton.value;
-      } else {
-        realCalculation += numberButton.innerText;
-      }
-    }
-    if (numberButton.innerText === "=") {
-      realCalculation = eval(calculationSpan.innerText);
-    }
-    if (numberButton.innerText === "AC") {
-      realCalculation = "";
-    }
-    if (numberButton.value === "del") {
-      realCalculation = realCalculation
-        .toString()
-        .slice(0, realCalculation.length - 1);
-      console.log(realCalculation.slice(0, realCalculation.length - 1));
-    }
-    if (realCalculation === Infinity) {
-      realCalculation = "Math Error";
-    }
-    calculationSpan.innerText = realCalculation;
-  });
+/**
+ * Does some calculations using eval (might be dangerous)
+ * @param {number} num1
+ * @param {number} num2
+ * @param {string} operator
+ */
+function calc(num1, num2, operator) {
+  return eval(`${num1} ${operator} ${num2}`);
 }
+
+function main() {
+  for (let numberButton of numberButtons) {
+    numberButton.addEventListener("click", () => {
+      if (
+        numberButton.value !== "=" &&
+        numberButton.value !== "AC" &&
+        numberButton.value !== "del"
+      ) {
+        // Because we're using the attribute value for buttons with operators (or anything that's not a number) we have to differentiate between between them with this if statement
+        if (numberButton.classList.contains("operator")) {
+          realCalculation += numberButton.value;
+        } else {
+          realCalculation += numberButton.innerText;
+        }
+      }
+      if (numberButton.value === "=") {
+        realCalculation = eval(calculationSpan.innerText);
+      }
+      if (numberButton.innerText === "AC") {
+        realCalculation = "";
+      }
+      if (numberButton.value === "del") {
+        realCalculation = realCalculation.slice(0, realCalculation.length - 1);
+      }
+      if (realCalculation === Infinity) {
+        realCalculation = "Math Error";
+      }
+      calculationSpan.innerText = realCalculation;
+    });
+  }
+}
+
+main();
